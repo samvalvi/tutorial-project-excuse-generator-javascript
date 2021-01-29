5aaae525474611a7b49c86a9dd9d033168d3a480
samvalvi <57542220+samvalvi@users.noreply.github.com>
2021-01-29 13:48:46 -0600

    Excuse Generator finalized.
    Last commit.

---
+++ excuses.js
@@ -0,0 +1,16 @@
+let who = ['the dog','my granma','his turtle','my bird'];
+let what = ['eat','pissed','crushed','broked'];
+let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
+
+const onLoad = () => {
+    
+    let firstWord = Math.floor((Math.random() * who.length) + 0);
+    let secondWord = Math.floor((Math.random() * what.length) + 0);
+    let thirdWord = Math.floor((Math.random() * when.length) + 0);
+    
+    let str = who [firstWord] + ' ' + what[secondWord] + ' ' + when[thirdWord] + '.'
+    let sentence = str.toString().charAt(0).toLocaleUpperCase() + str.slice(1);
+
+    let container = document.getElementById('excuse');
+    container.innerHTML = sentence;         
+}
