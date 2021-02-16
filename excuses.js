29b5631f234dee7d57242c1863ffa05583d57d9d
samvalvi <samuelvalerin@gmail.com>
2021-02-16 17:30:07 +0000

    last commit

--- excuses.js
+++ excuses.js
@@ -2,8 +2,9 @@ let who = ['the dog','my granma','his turtle','my bird'];
 let what = ['eat','pissed','crushed','broked'];
 let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
 
-const onLoad = () => {
+window.onLoad = () => {
     
+    //Choose a random index
     let firstWord = Math.floor((Math.random() * who.length) + 0);
     let secondWord = Math.floor((Math.random() * what.length) + 0);
     let thirdWord = Math.floor((Math.random() * when.length) + 0);
